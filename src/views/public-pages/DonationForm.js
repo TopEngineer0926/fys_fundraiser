31092895a71c5a7be3f0b19e64fe8e5c2ef2b00f
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-04-19 03:21:17 +0500

    WIP

--- src/views/public-pages/DonationForm.js
+++ src/views/public-pages/DonationForm.js
@@ -14,10 +14,12 @@ import {
   InputGroupText,
   Label
 } from 'reactstrap'
-// import { loadStripe } from "@stripe/stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
 import { Elements } from "@stripe/react-stripe-js"
 import Footer from './Footer'
 import NavBar from './NavBar'
+import axios from 'axios'
+
 const stripePromise = loadStripe(`${process.env.REACT_APP_BASE_URL}`)
 
 const DonationForm = () => {
@@ -31,13 +33,15 @@ const DonationForm = () => {
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch(`${process.env.REACT_APP_BASE_URL}/create-payment-intent`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] })
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret))
+    // await axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/stripe/pi`, {})
+    axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/stripe/pi`, {
+      cardAmount: 2000,
+      transferGroup: "transfer123",
+      items: "asdf"
+  })
+      .then((data) => { 
+        setClientSecret(data.data.clientSecret)
+})
   }, [])
   const appearance = {
     theme: 'stripe'
