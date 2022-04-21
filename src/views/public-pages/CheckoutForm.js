7e5b6a3fbc7100892e952361dd907a9455bbfe8b
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-04-20 20:48:39 -0700

    Fixed hard-coded link

--- src/views/public-pages/CheckoutForm.js
+++ src/views/public-pages/CheckoutForm.js
@@ -64,7 +64,7 @@ export default function CheckoutForm(props) {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: 'http://localhost:3000/campaign/thankyou'
+                return_url: 'https://app.fundyouthsports.com/campaign/thankyou'
             }
         })
 
