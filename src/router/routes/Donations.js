268270c41450e70dfaa349e1c7e71dda43371ecc
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-04-21 06:23:50 -0700

    Updated DOnation Routes

--- src/router/routes/Donations.js
+++ src/router/routes/Donations.js
@@ -6,6 +6,7 @@ import { Navigate } from 'react-router-dom'
 const DonationList = lazy(() => import('../../views/donations/list'))
 const DonationView = lazy(() => import('../../views/donations/view'))
 const DonationPublicView = lazy(() => import('../../views/donations/public'))
+const PublicThankYou = lazy(() => import('../../views/public-pages/ThankYou'))
 
 const DonationRoutes = [
   {
@@ -23,6 +24,15 @@ const DonationRoutes = [
   {
     path: '/donations/:donation_slug',
     element: <DonationPublicView />, 
+    meta: {
+      layout: 'blank',
+      publicRoute: true,
+      restricted: false
+    }
+  },
+  {
+    path: '/donations/:donation_slug/thank-you',
+    element: <PublicThankYou />, 
     meta: {
       layout: 'blank',
       publicRoute: true,
