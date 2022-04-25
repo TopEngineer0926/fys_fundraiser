25fe18e6b5b1f6e96b075fc72a064dd4a1608b76
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-04-25 10:23:15 -0700

    Marker.io setup

--- src/@core/layouts/components/footer/index.js
+++ src/@core/layouts/components/footer/index.js
@@ -1,8 +1,14 @@
 // ** Icons Import
 import { Heart } from 'react-feather'
+import markerSDK from '@marker.io/browser'
 
 const Footer = () => {
-  return (
+
+  markerSDK.loadWidget({
+    destination: '6266ca071e1f6f52f3baa1bd'
+  })
+  
+    return (
     <p className='clearfix mb-0'>
       <span className='float-md-start d-block d-md-inline-block mt-25'>
         COPYRIGHT © {new Date().getFullYear()}{' '}
