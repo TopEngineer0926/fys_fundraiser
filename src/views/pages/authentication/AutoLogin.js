e03a4a99efac90a5f261533214b4c6de817450ba
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-05-12 17:10:44 +0530

    2xb61n3-User Forgot Password Link Alert

--- src/views/pages/authentication/AutoLogin.js
+++ src/views/pages/authentication/AutoLogin.js
@@ -3,7 +3,7 @@ import '@styles/react/pages/page-authentication.scss'
 import { Link, useSearchParams, useNavigate } from 'react-router-dom'
 
 import axios from 'axios'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AbilityContext } from '@src/utility/context/Can'
 
 import { useDispatch } from 'react-redux'
@@ -28,6 +28,7 @@ import {
 } from 'react-feather'
 
 const AutoLogin = () => {
+  const [error, setError] = useState("")
   const [searchParams] = useSearchParams()
   const uuid = searchParams.get("uuid")
   const token = searchParams.get("token")
@@ -69,31 +70,39 @@ const AutoLogin = () => {
   }
 
   if (uuid && token) {
-        axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/user/otl`, { uuid: searchParams.get("uuid"), token: searchParams.get("token") })
-          .then(res => {
-            if (!(res.data.hasError)) {
-              const data = { ...res.data.data, accessToken: res.data.data.token, refreshToken: res.data.data.token, abilities: res?.data?.data?.abilities }
-              dispatch(handleLogin(data))
-              ability.update(res.data.data?.abilities)
-              navigate(getHomeRouteForLoggedInUser(res.data.data.role || 'admin'))
-              toast(t => (
-                <ToastContent t={t} role={data.role || 'admin'} name={res.data.data.email || res.data.data.username} />
-              ))
-            } else {
-              toast(t => (
-                <ToastError t={t} />
-              ))
-            }
-          })
-          .catch(err => console.log(err))
-      } else {
-        for (const key in data) {
-          if (data[key].length === 0) {
-            setError(key, {
-              type: 'manual'
-            })
+    axios.post(`${process.env.REACT_APP_BASE_URL}/api/v1/user/otl`, { uuid: searchParams.get("uuid"), token: searchParams.get("token") })
+      .then(res => {
+        if (!(res.data.hasError)) {
+          const data = { ...res.data.data, accessToken: res.data.data.token, refreshToken: res.data.data.token, abilities: res?.data?.data?.abilities }
+          dispatch(handleLogin(data))
+          ability.update(res.data.data?.abilities)
+          navigate(getHomeRouteForLoggedInUser(res.data.data.role || 'admin'))
+          toast(t => (
+            <ToastContent t={t} role={data.role || 'admin'} name={res.data.data.email || res.data.data.username} />
+          ))
+        } else {
+          toast(t => (
+            <ToastError t={t} />
+          ))
+        }
+      })
+      .catch(err => {
+        if (err.response.data.hasError) {
+          if (err.response.data.data.message) {
+            setError(err.response.data.data?.message)
+          } else {
+            setError('Something went wrong. Please try again!')
           }
-        }    
+        }
+      })
+  } else {
+    for (const key in data) {
+      if (data[key].length === 0) {
+        setError(key, {
+          type: 'manual'
+        })
+      }
+    }    
   }
 
   return (
@@ -102,7 +111,7 @@ const AutoLogin = () => {
         <Card className='mb-0'>
           <CardBody>
             <Link className='brand-logo' to='/' onClick={e => e.preventDefault()}>
-              <img src={require('@src/assets/images/logo/FYS-horizontal.png').default} style={{width: "auto", height: "30px"}}></img>
+              <img src={require('@src/assets/images/logo/FYS-horizontal.png').default} style={{ width: "auto", height: "30px" }}></img>
             </Link>
             <CardTitle tag='h2' className='fw-bolder mb-1'>
               Logging you in :)
@@ -110,8 +119,14 @@ const AutoLogin = () => {
             <CardText className='mb-2'>
               Please wait a moment while we validate your link and log you into your account.
             </CardText>
+            <span className="d-flex justify-content-center text-danger mb-1 fs-4 font-weight-bold">{error}</span>
             <p className='text-center mt-2'>
               <span>Didn't log you in? </span>
+              <a href='/login'>
+                <span>Return to Login</span>
+              </a>
+            </p>
+            <p className='text-center mt-2'>
               <a href='/forgot-password'>
                 <span>Resend your confirmation link</span>
               </a>
